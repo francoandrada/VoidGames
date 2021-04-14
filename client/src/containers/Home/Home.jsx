7b118c653d67024c3ae774b2cf733ece9e29300a
viccoronado <vcoronadoasm@gmail.com>
2021-04-14 10:19:50 -0300

    Updating with translations

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { showVideogames, resetAll } from "../../actions/index";
 import Videogame from "../Videogame/Videogame";
-import Loading from "../../components/Loading/Loading"
+import Loading from "../../components/Loading/Loading";
 import { Pagination } from "../../components/Pagination/Pagination";
 import { Filter } from "../../components/Filter/Filter";
+import { useTranslation } from "react-i18next";
 import "./Home.css";
-
 
 function Home() {
   const dispatch = useDispatch();
@@ -16,10 +16,11 @@ function Home() {
   const loading = useSelector((state) => state.loading);
   const orderBy = useSelector((state) => state.orderBy);
   const videogames = useSelector((state) => state.videogames);
+  const [t, i18n] = useTranslation("global");
   let allVideogames;
 
   useEffect(() => {
-    dispatch(resetAll())
+    dispatch(resetAll());
     dispatch(showVideogames());
     // eslint-disable-next-line
   }, []);
@@ -47,15 +48,15 @@ function Home() {
   let currentPage = allVideogames.slice(indexFirtsPage, indexLastPage);
   // videogames of the current page
 
-
   return (
     <div className="home">
       <>
-        {loading ? <Loading />
-          :
+        {loading ? (
+          <Loading />
+        ) : (
           <>
             <div className="title">
-              <h1>All videogames</h1>
+              <h1>{t("home.title")}</h1>
               <Filter />
               <Pagination
                 videogamesPerPage={videogamesPerPage}
@@ -65,11 +66,10 @@ function Home() {
               <Videogame videogames={currentPage} />
             </div>
           </>
-        }
+        )}
       </>
     </div>
-  )
-};
-
+  );
+}
 
 export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,31 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import i18next from "i18next";
+import global_es from "./traslations/ES/global.json";
+import global_en from "./traslations/EN/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <I18nextProvider i18n={i18next}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </I18nextProvider>,
   document.getElementById("root")
 );
